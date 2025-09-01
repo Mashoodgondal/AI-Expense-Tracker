f72e7beb3e5e60da135e3807b3aaed0505af01c2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-01 22:53:25 +0500

    starting new project Expense tracker

---
+++ ai_expence/components/navebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+const Navebar = () => {
+    return (
+        <div><UserButton></UserButton></div>
+    )
+}
+
+export default Navebar
