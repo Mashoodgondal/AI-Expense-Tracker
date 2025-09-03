ca1b72caf6dd93648750c74b431ca8d0b4db06d6
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-03 18:40:05 +0500

    add prisma

--- ai_expence/components/navebar.tsx
+++ ai_expence/components/navebar.tsx
@@ -1,13 +1,3 @@
-// import React from 'react'
-// import { UserButton } from '@clerk/nextjs'
-// const Navebar = () => {
-//     return (
-//         <div><UserButton></UserButton></div>
-//     )
-// }
-
-// export default Navebar
-
 "use client";
 import { useState } from "react";
 import { CiMenuFries } from "react-icons/ci";
