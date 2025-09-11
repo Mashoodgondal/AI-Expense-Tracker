5d8239b50fb30e48838d97ad708a48a03cefe84f
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-11 22:30:10 +0500

    giving right paths

--- ai_expence/app/page.tsx
+++ ai_expence/app/page.tsx
@@ -1,10 +1,12 @@
 
-import Image from "next/image";
+import Guest from "@/components/Guest";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      hellow word
+      <Guest />
+
     </div>
   );
 }
