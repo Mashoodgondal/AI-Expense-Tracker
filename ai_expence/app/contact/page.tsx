60c66855e3be64720f08a467e1861def0067861e
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-12 22:50:55 +0500

    add about page

--- ai_expence/app/contact/page.tsx
+++ ai_expence/app/contact/page.tsx
@@ -1,13 +1,3 @@
-// import React from 'react'
-
-// const ContactPage = () => {
-//     return (
-//         <div>ContactPage</div>
-//     )
-// }
-
-// export default ContactPage
-
 'use client';
 
 const ContactPage = () => {
