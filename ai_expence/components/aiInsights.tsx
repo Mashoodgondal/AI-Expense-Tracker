dc696d59e82e0d55e57395b720b96d913bd446cd
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-23 21:55:40 +0500

    commit ai aligns

--- ai_expence/components/aiInsights.tsx
+++ ai_expence/components/aiInsights.tsx
@@ -18,6 +18,13 @@ const AIInsights = () => {
             here is the page to add ai insights in project
             here is the page to add ai insights in project
             here is the page to add ai insights in project
+            here is the page to add ai insights in project  here is the page to add ai insights in project
+            here is the page to add ai insights in project
+            here is the page to add ai insights in project
+            here is the page to add ai insights in project
+            here is the page to add ai insights in project
+            here is the page to add ai insights in project
+            here is the page to add ai insights in project
             here is the page to add ai insights in project
         </div>
     )
