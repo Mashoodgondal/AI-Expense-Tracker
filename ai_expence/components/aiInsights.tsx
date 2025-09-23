06f5483feda81f9d191484d93ab847b5f26255f0
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-23 21:56:14 +0500

    commit ai aligns

--- ai_expence/components/aiInsights.tsx
+++ ai_expence/components/aiInsights.tsx
@@ -3,12 +3,6 @@ import React from "react"
 const AIInsights = () => {
     return (
         <div>AIInsights
-            here is the page to add ai insights in project
-            here is the page to add ai insights in project
-            here is the page to add ai insights in project
-            here is the page to add ai insights in project
-            here is the page to add ai insights in project
-            here is the page to add ai insights in project
 
         </div>
     )
