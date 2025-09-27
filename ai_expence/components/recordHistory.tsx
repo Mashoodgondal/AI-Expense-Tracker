c9b4dbb504066f12127f3492c8df57314058ea62
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-09-27 21:03:01 +0500

    add records

--- ai_expence/components/recordHistory.tsx
+++ ai_expence/components/recordHistory.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const RecordHistory = () => {
     return (
-        <div>RecordHistory</div>
+        <div>RecordHistory
+            Adding some data for record history in project
+        </div>
     )
 }
 
